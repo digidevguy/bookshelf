fe1ce5923158ebc62479ec37da351bd8e2a2c850
Matthew <litmatthew85@gmail.com>
2023-09-13 13:58:47 -0500

    Adding conditional rendering and Google log in

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,30 @@
+'use client';
+import {
+	Session,
+	createClientComponentClient,
+} from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState } from 'react';
+
 export default function Account() {
+	const [session, setSession] = useState<Session>();
+	const supabase = createClientComponentClient();
+
+	useEffect(() => {
+		const fetchSession = async () => {
+			const {
+				data: { session },
+			} = await supabase.auth.getSession();
+			if (session) {
+				setSession(session);
+			}
+		};
+		fetchSession();
+	});
+
 	return (
 		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
 			<h1 className='text-4xl font-bold text-center'>Account page</h1>
+			<div></div>
 		</main>
 	);
 }
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -1,18 +1,72 @@
 'use client';
 import Link from 'next/link';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import {
+	Session,
+	createClientComponentClient,
+} from '@supabase/auth-helpers-nextjs';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function NavBar() {
 	const supabase = createClientComponentClient();
+	const [session, setSession] = useState<Session | null>(null);
 	const router = useRouter();
+
+	useEffect(() => {
+		async function getSession() {
+			const {
+				data: { session },
+				error,
+			} = await supabase.auth.getSession();
+
+			if (session) {
+				setSession(session);
+			}
+		}
+		getSession();
+	});
+
+	async function handleGoogleSignIn() {
+		const { data, error } = await supabase.auth.signInWithOAuth({
+			provider: 'google',
+			options: {
+				queryParams: {
+					access_type: 'offline',
+					prompt: 'consent',
+				},
+			},
+		});
+		if (error) {
+			console.error(error);
+			return;
+		}
+		console.log('Data: ', data);
+		setTimeout(() => {
+			router.push('/account'), 2000;
+		});
+	}
+
+	async function handleSignOut() {
+		const { error } = await supabase.auth.signOut();
+		if (error) {
+			console.error(error);
+			return;
+		}
+		setSession(null);
+		setTimeout(() => {
+			router.push('/'), 2000;
+		});
+	}
 
 	return (
 		<nav className='flex flex-row space-x-4 p-4'>
 			<Link href='/'>Home</Link>
 			<Link href='/account'>Account</Link>
-			<button>Sign in with Google</button>
+			{session ? (
+				<button onClick={handleSignOut}>Sign out</button>
+			) : (
+				<button onClick={handleGoogleSignIn}>Sign in with Google</button>
+			)}
 		</nav>
 	);
 }
