210fb68cd8343d5652f1aeb30b32d1fce1000e66
Matthew <litmatthew85@gmail.com>
2023-09-13 19:54:25 -0500

    Testing initial book search page and route handler.

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,17 +3,24 @@ import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
 	const { searchParams } = new URL(request.url);
 	const query = searchParams.get('q');
+	// replace spaces in query with + sign
+
+	console.log(
+		`https://www.googleapis.com/books/v1/volumes?q=${query}+intitle&maxResults=20`
+	);
 
 	try {
 		const response = await fetch(
-			`https://www.googleapis.com/books/v1/volumes?q=${query}+intitle`
+			`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=20`
 		);
 
-		const data = await response.json();
+		const { totalItems, items } = await response.json();
 
-		return NextResponse.json(data);
+		return NextResponse.json({ totalItems, items });
 	} catch (error: any) {
 		console.log('Error: ', error);
 		return NextResponse.json({ error: error.message });
 	}
 }
+
+('https://www.google.com/search?tbm=bks&q=the+way+of+kings');
--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,26 +1,46 @@
 'use client';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { use, useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
 
 export default function BookSearchPage() {
 	const [searchTerm, setSearchTerm] = useState<string>('');
 	const [searchResults, setSearchResults] = useState<any[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const [numResults, setNumResults] = useState<number>(0);
 
 	function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
 		setSearchTerm((prev) => event.target.value);
 	}
 
+	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		try {
+			const results = await fetch(`/api/search?q=${searchTerm}`);
+			const { totalItems, items } = await results.json();
+			setSearchResults(items);
+			setNumResults(totalItems);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	useEffect(() => console.log(searchResults), [searchResults]);
+
 	return (
 		<main className='flex min-h-screen flex-col items-center p-24 space-y-4'>
 			<h1 className='text-4xl font-bold text-center'>Book Search Page</h1>
-			<form className='flex flex-col border border-slate-400 p-2 rounded-md'>
+			<form
+				className='flex flex-col border border-slate-400 p-2 rounded-md'
+				onSubmit={handleSubmit}
+			>
 				<label htmlFor='book-search'>Search for a book</label>
 				<div className='flex flex-row space-x-2 justify-center items-center '>
 					<input
 						type='text'
 						id='book-search'
 						name='book-search'
-						className='border-2 border-gray-500 rounded-md'
+						className='border-2 border-gray-500 rounded-md p-2'
 						value={searchTerm}
 						onChange={handleInputChange}
 					/>
@@ -29,6 +49,34 @@ export default function BookSearchPage() {
 					</button>
 				</div>
 			</form>
+			{numResults ? (
+				<div>
+					<p>{numResults}</p>
+				</div>
+			) : null}
+			<div className='flex flex-col space-y-4 p-2'>
+				{searchResults.map((book) => (
+					<div key={book.id} className='flex flex-row space-x-4 items-center'>
+						<div className='w-32 h-48'>
+							<Image
+								src={book.volumeInfo.imageLinks.thumbnail}
+								alt={book.volumeInfo.title}
+								// width={128}
+								// height={193}
+								fill
+								style={{ objectFit: 'cover' }}
+							/>
+						</div>
+						<div>
+							<h2 className='text-2xl font-bold'>{book.volumeInfo.title}</h2>
+							<h3 className='text-md font-semibold'>
+								{book.volumeInfo.subtitle}
+							</h3>
+							<p>{book.volumeInfo.authors}</p>
+						</div>
+					</div>
+				))}
+			</div>
 		</main>
 	);
 }
